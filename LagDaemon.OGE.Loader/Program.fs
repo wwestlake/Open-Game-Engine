b78ef1b12ef12d99e7ec1dc325c2723d6ec25fa1
wwestlake <wwestlake@lagdaemon.com>
2016-11-16 16:23:42 -0500

    github

--- LagDaemon.OGE.Loader/Program.fs
+++ LagDaemon.OGE.Loader/Program.fs
@@ -16,7 +16,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    Source code available at: https://github.com/wwestlake/KnightsTour
+    Source code available at: https://github.com/wwestlake/Open-Game-Engine
 *)  
 open System
 open System.Drawing
