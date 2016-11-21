bb8afe6935836fa0bf761de0bdf713143f73907a
wwestlake <wwestlake@lagdaemon.com>
2016-11-21 13:48:02 -0500

    Create GeneralFunctions.fs

---
+++ LagDaemon.OGE.InterfaceTypes/GeneralFunctions.fs
@@ -0,0 +1,22 @@
+namespace LagDaemon.OGE.InterfaceTypes
+
+  module LibFuncs =
+  
+    let hook observer f param =
+      let y = f param
+      observer y
+      y
+     
+    let hook2 observer f param1 param2 =
+      let y = f param1 param2
+      observer y
+      y
+    
+    let hook3 observer f param1 param2 param3 =
+      let y = f param1 param2 param3
+      observer y
+      y
+      
+    
+    
+    
